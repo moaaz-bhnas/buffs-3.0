803eb5a76d6458ae1af10121dbd644956150c675
Moaaz Bhnas <moaaz_bs@yahoo.com>
2023-04-23 12:21:49 +0200

    Added user's avatar to header

--- src/components/header/DesktopNavigation.tsx
+++ src/components/header/DesktopNavigation.tsx
@@ -5,49 +5,76 @@ import {
   UserIcon,
 } from "@heroicons/react/24/outline";
 import SearchForm from "./SearchForm";
+import { authOptions, getCurrentUser } from "@/lib/next-auth";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 
 type Props = {};
 
-function DesktopNavigation({}: Props) {
+async function DesktopNavigation({}: Props) {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect(authOptions?.pages?.signIn || "/login");
+  }
+
   return (
-    <div className="flex items-center gap-x-3.5">
+    <div className="flex items-center gap-x-3">
       {/* Search bar */}
       <div className="flex-1">
         <SearchForm />
       </div>
 
-      <ul className="flex gap-x-1 -me-2.5">
+      <ul className="-me-2.5 flex gap-x-1">
         {/* Notifications */}
         <li>
           <button
-            className="w-12 h-12 flex"
+            className="flex h-12 w-12 items-center justify-center rounded-full duration-75 hover:bg-gray-200 focus:bg-gray-200"
             type="button"
             aria-label="Toggle notifications panel"
           >
-            <BellIcon className="w-7 m-auto" />
+            <BellIcon className="w-7" />
           </button>
         </li>
 
         {/* Inbox */}
         <li>
           <button
-            className="w-12 h-12 flex"
+            className="flex h-12 w-12 items-center justify-center rounded-full duration-75 hover:bg-gray-200 focus:bg-gray-200"
             type="button"
             aria-label="Toggle messages panel"
           >
-            <PaperAirplaneIcon className="w-7 m-auto" />
+            <PaperAirplaneIcon className="w-7" />
           </button>
         </li>
 
         {/* Profile */}
         <li>
-          <button
-            className="w-12 h-12 flex items-center justify-center"
-            type="button"
-            aria-label="Toggle profile settings"
+          <Link
+            href="/profile"
+            className="flex h-12 w-12 items-center justify-center rounded-full duration-75 hover:bg-gray-200 focus:bg-gray-200"
+            aria-label="Your profile page"
           >
-            <UserIcon className="w-7" />
-            <ChevronDownIcon className="w-3.5" />
+            <Image
+              className="w-7 rounded-full"
+              src={user.image ? user.image : "Placeholder"}
+              alt=""
+              width={0}
+              height={0}
+              sizes="2rem"
+            />
+          </Link>
+        </li>
+
+        {/* Settings */}
+        <li>
+          <button
+            className="flex h-12 w-12 items-center justify-center rounded-full duration-75 hover:bg-gray-200 focus:bg-gray-200"
+            type="button"
+            aria-label="Toggle settings panel"
+          >
+            <ChevronDownIcon className="w-7" />
           </button>
         </li>
       </ul>

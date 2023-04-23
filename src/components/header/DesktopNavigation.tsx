288d890cc20842b1c2c7b0e8d1425d7c3853a509
Moaaz Bhnas <moaaz_bs@yahoo.com>
2023-04-23 10:16:36 +0200

    Finished Header for Desktop

--- src/components/header/DesktopNavigation.tsx
+++ src/components/header/DesktopNavigation.tsx
@@ -1,4 +1,9 @@
-import { BellIcon, PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import {
+  BellIcon,
+  ChevronDownIcon,
+  PaperAirplaneIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 import SearchForm from "./SearchForm";
 
 type Props = {};
@@ -13,22 +18,38 @@ function DesktopNavigation({}: Props) {
 
       <ul className="flex gap-x-1 -me-2.5">
         {/* Notifications */}
-        <button
-          className="w-12 h-12 flex"
-          type="button"
-          aria-label="Toggle notifications panel"
-        >
-          <BellIcon className="w-7 m-auto" />
-        </button>
+        <li>
+          <button
+            className="w-12 h-12 flex"
+            type="button"
+            aria-label="Toggle notifications panel"
+          >
+            <BellIcon className="w-7 m-auto" />
+          </button>
+        </li>
 
         {/* Inbox */}
-        <button
-          className="w-12 h-12 flex"
-          type="button"
-          aria-label="Toggle messages panel"
-        >
-          <PaperAirplaneIcon className="w-7 m-auto" />
-        </button>
+        <li>
+          <button
+            className="w-12 h-12 flex"
+            type="button"
+            aria-label="Toggle messages panel"
+          >
+            <PaperAirplaneIcon className="w-7 m-auto" />
+          </button>
+        </li>
+
+        {/* Profile */}
+        <li>
+          <button
+            className="w-12 h-12 flex items-center justify-center"
+            type="button"
+            aria-label="Toggle profile settings"
+          >
+            <UserIcon className="w-7" />
+            <ChevronDownIcon className="w-3.5" />
+          </button>
+        </li>
       </ul>
     </div>
   );
--- src/components/header/MobileNavigation.tsx
+++ src/components/header/MobileNavigation.tsx
@@ -1,9 +1,18 @@
 import Link from "next/link";
-import { BellIcon, PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import {
+  BellIcon,
+  MagnifyingGlassIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/24/outline";
 
 type Props = {};
 
 const navigationLinks = [
+  {
+    label: "Search",
+    href: "/search",
+    Icon: MagnifyingGlassIcon,
+  },
   {
     label: "Notifications",
     href: "/notifications",
@@ -21,14 +30,11 @@ function MobileNavigation({}: Props) {
     <ul className="flex gap-x-1 -me-2.5">
       {/* (-me-2.5) to align the end of the last icon with content below */}
       {navigationLinks.map(({ label, href, Icon }) => (
-        <Link
-          className="w-12 h-12 flex"
-          aria-label={label}
-          key={label}
-          href={href}
-        >
-          <Icon className="w-7 m-auto" />
-        </Link>
+        <li key={label}>
+          <Link className="w-12 h-12 flex" aria-label={label} href={href}>
+            <Icon className="w-7 m-auto" />
+          </Link>
+        </li>
       ))}
     </ul>
   );

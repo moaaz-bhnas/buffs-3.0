09b2802cb65a2f93b0a90caaed9acbad07c2beab
Moaaz Bhnas <111786023+moaazbhnas228@users.noreply.github.com>
2023-05-31 05:32:55 +0200

    WIP

--- src/components/inline-input/InlineInput.tsx
+++ src/components/inline-input/InlineInput.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import classNames from "@/utils/style/classNames";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 
 type Props = {
   type: string;
   label: string;
+  value: string;
+  onChange: Dispatch<SetStateAction<string>>;
   classname?: string;
   labelClassName?: string;
 };
@@ -13,6 +15,8 @@ type Props = {
 function InlineInput({
   type,
   label,
+  value,
+  onChange,
   classname = "",
   labelClassName = "",
 }: Props) {
@@ -24,13 +28,15 @@ function InlineInput({
         className={classNames(
           "absolute left-1 self-center bg-white px-1 transition-all",
           labelClassName,
-          inputFocused ? "-top-4 text-sm" : "top-1/2 -translate-y-1/2"
+          inputFocused || value ? "-top-4 text-sm" : "top-1/2 -translate-y-1/2"
         )}
       >
         {label}
       </span>
       <input
         className={classNames("w-full border", classname)}
+        value={value}
+        onChange={(event) => onChange(event.target.value)}
         type={type}
         onFocus={() => setInputFocused(true)}
         onBlur={() => setInputFocused(false)}
---
+++ src/interfaces/database/User.ts
@@ -0,0 +1,7 @@
+export interface RegisteringDBUser {
+  username: string;
+  displayName: string;
+  email: string;
+  password: string;
+  role: string;
+}

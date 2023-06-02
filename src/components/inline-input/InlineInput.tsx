1312caf2bdb1f3bb10f16e5a3565da4f3b382361
Moaaz Bhnas <moaaz_bs@yahoo.com>
2023-06-03 00:44:38 +0300

    Added loading state to signup button

--- src/components/inline-input/InlineInput.tsx
+++ src/components/inline-input/InlineInput.tsx
@@ -28,7 +28,9 @@ function InlineInput({
         className={classNames(
           "absolute left-1 self-center bg-white px-1 transition-all",
           labelClassName,
-          inputFocused || value ? "-top-3 text-xs" : "top-1/2 -translate-y-1/2"
+          inputFocused || value
+            ? "-top-2.5 text-xs"
+            : "top-1/2 -translate-y-1/2"
         )}
       >
         {label}

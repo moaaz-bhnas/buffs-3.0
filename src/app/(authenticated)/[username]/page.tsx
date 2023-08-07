c6f313ad546592df2ae6cd094ecc451074c1c605
toony10 <amiranton257@gmail.com>
2023-08-08 00:28:40 +0300

    profile page header

--- src/app/(authenticated)/[username]/page.tsx
+++ src/app/(authenticated)/[username]/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import Container from "@/components/container/Container";
 import { PageProps } from "../../../../.next/types/app/(authenticated)/[username]/page";
 import { ServerApiClient } from "@/apis/server-api-client";
 import Avatar from "@/components/avatar/Avatar";
+import ThemeButton from "@/components/theme-button/ThemeButton";
+import classNames from "../../../helpers/style/classNames";
 
 export default async function page({ params }: PageProps) {
   const serverApiClient = new ServerApiClient();
@@ -12,8 +16,46 @@ export default async function page({ params }: PageProps) {
   return (
     <Container>
       <div className="flex">
-        <Avatar avatarUrl={userResult.value.avatar}></Avatar>
-        <h1> {userResult.value.displayName}</h1>
+        <div className="w-1/4">
+          <Avatar avatarUrl={userResult.value.avatar} size={150}></Avatar>
+        </div>
+        <div className="flex w-9/12 flex-col">
+          <div className="flex">
+            <h1 className="font-medium text-lg w-1/2">
+              {userResult.value.displayName}
+            </h1>
+            <ThemeButton
+              type="button"
+              className="p-10 hover:bg-teal-500 font-bold">
+              Follow
+            </ThemeButton>
+          </div>
+          <div className="mt-5">
+            <ul className="flex justify-between w-full md:w-9/12">
+              <li className="text-base md:text-lg font-medium text-center">
+                <span className="font-bold">208 </span>reviews
+              </li>
+              <li className="text-base md:text-lg font-medium text-center">
+                <span className="font-bold">20K </span>followers
+              </li>
+              <li className="text-base md:text-lg font-medium text-center">
+                <span className="font-bold">400 </span>following
+              </li>
+            </ul>
+          </div>
+          <div className="mt-5 font-semibold">{userResult.value.username}</div>
+          <div>
+            🎬 Movie Lover 🍿 | 📝 Emojee Reviewer 🎭✨ | 🌟 Sharing My
+            Cinematic Emotions! 😍🎉 Welcome to my movie realm! 🎉 As a devoted
+            film enthusiast, I express my cinematic experiences through
+            captivating emojee reviews! 📝💖 From heartwarming to
+            adrenaline-pumping moments, I've got all the emojis to convey the
+            magic of movies! 🎥✨ Join me on this emotive journey as we uncover
+            cinematic treasures together! 🏆🎬 Let's create a wonderland of
+            emotions through movies and share our thoughts and recommendations!
+            🤗💬🍿
+          </div>
+        </div>
       </div>
     </Container>
   );

ebfed4e9065c2c95ea49d7d1efaa01b16501d678
toony10 <amiranton257@gmail.com>
2023-08-14 20:16:02 +0300

    profile page (final)

--- src/app/(authenticated)/[username]/page.tsx
+++ src/app/(authenticated)/[username]/page.tsx
@@ -1,12 +1,22 @@
 import Container from "@/components/container/Container";
 import ProfileHeader from "@/components/profile-header/ProfileHeader";
 import FavoriteMovies from "@/components/favorite-movies/FavoriteMovies";
+import { ServerApiClient } from "@/apis/server-api-client";
 
 export default async function page({
   params,
 }: {
   params: { username: string };
 }) {
+  const serverApiClient = new ServerApiClient();
+  const userResult = await serverApiClient.getUserByUsername(params.username);
+  if (userResult.isErr()) {
+    return (
+      <Container>
+        <div>Oops, User not found</div>
+      </Container>
+    );
+  }
   return (
     <main>
       <Container>

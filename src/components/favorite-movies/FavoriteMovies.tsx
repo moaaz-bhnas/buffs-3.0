8550b68e99f2f1b731ff6ece494653b541c70b69
toony10 <amiranton257@gmail.com>
2023-08-16 22:25:14 +0300

    ul

--- src/components/favorite-movies/FavoriteMovies.tsx
+++ src/components/favorite-movies/FavoriteMovies.tsx
@@ -7,17 +7,17 @@ function FavoriteMovies() {
         FAVORITE FILMS
       </h1>
       <hr className="h-px my-3 bg-gray-400 border-0" />
-      <div className="flex gap-3">
-        <div className="w-1/3 h-48 md:h-[22rem] lg:h-96 bg-slate-300 rounded-md flex justify-center group hover:drop-shadow-xl ease-in duration-150 cursor-pointer">
+      <ul className="flex gap-3">
+        <li className="w-1/3 h-48 md:h-[22rem] lg:h-96 bg-slate-300 rounded-md flex justify-center group hover:drop-shadow-xl ease-in duration-150 cursor-pointer">
           <PlusIcon className="w-12 h-12 md:w-20 md:h-20 m-auto opacity-20 group-hover:opacity-90 transition-opacity ease-in duration-150" />
-        </div>
-        <div className="w-1/3 h-48 md:h-[22rem] lg:h-96 bg-slate-300 rounded-md flex justify-center group hover:drop-shadow-xl ease-in duration-150 cursor-pointer">
+        </li>
+        <li className="w-1/3 h-48 md:h-[22rem] lg:h-96 bg-slate-300 rounded-md flex justify-center group hover:drop-shadow-xl ease-in duration-150 cursor-pointer">
           <PlusIcon className="w-12 h-12 md:w-20 md:h-20 m-auto opacity-20 group-hover:opacity-90 transition-opacity ease-in duration-150" />
-        </div>
-        <div className="w-1/3 h-48 md:h-[22rem] lg:h-96 bg-slate-300 rounded-md flex justify-center group hover:drop-shadow-xl ease-in duration-150 cursor-pointer">
+        </li>
+        <li className="w-1/3 h-48 md:h-[22rem] lg:h-96 bg-slate-300 rounded-md flex justify-center group hover:drop-shadow-xl ease-in duration-150 cursor-pointer">
           <PlusIcon className="w-12 h-12 md:w-20 md:h-20 m-auto opacity-20 group-hover:opacity-90 transition-opacity ease-in duration-150" />
-        </div>
-      </div>
+        </li>
+      </ul>
     </div>
   );
 }

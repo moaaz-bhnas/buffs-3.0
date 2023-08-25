65ed1a7857bcce5352520f6a9c776cdcd87dd015
Moaaz Bhnas <moaaz_bs@yahoo.com>
2023-08-25 12:49:02 +0300

    Added progress bar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/header/Header";
 import "@/app/globals.css";
+import NextTopLoader from "nextjs-toploader";
 
 export default function RootLayout({
   children,
@@ -9,6 +10,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <NextTopLoader
+          color="rgb(45 212 191)" // teal-400
+          initialPosition={0.08}
+          height={3}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+        />
         <Header />
         {children}
       </body>
--- src/app/(un-authenticated)/layout.tsx
+++ src/app/(un-authenticated)/layout.tsx
@@ -5,6 +5,7 @@ import Logo from "@/components/logo/Logo";
 import classNames from "@/helpers/style/classNames";
 
 import { Open_Sans } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 
 // display: https://developer.mozilla.org/en-US/docs/Web/CSS/@font-face/font-display
 const openSans = Open_Sans({
@@ -21,6 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en" className={classNames(openSans.variable)}>
       <body className="font-open">
+        <NextTopLoader
+          color="rgb(45 212 191)" // teal-400
+          initialPosition={0.08}
+          height={3}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+        />
         <Container>
           <Logo className="w-14" />
         </Container>

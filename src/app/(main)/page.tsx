db1b90aab5568a2759138c948870a44dde0966c1
Moaaz Bhnas <moaaz_bs@yahoo.com>
2023-09-02 15:34:59 +0300

    Done

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,13 +3,26 @@ import AddReviewSection from "@/components/add-review/desktop/AddReviewSection";
 import taglineMessages from "@/utils/messages/taglineMessages";
 import { Metadata } from "next/types";
 import Feed from "@/components/feed/Feed";
+import getServerToken from "@/helpers/auth/getServerToken";
+import { ServerApiClient } from "@/apis/server-api-client";
+import getServerUser from "@/helpers/auth/getServerUser";
 
 export const metadata: Metadata = {
   title: "Feed | Buffs",
   description: taglineMessages.default,
 };
 
-export default function Home() {
+const serverApiClient = new ServerApiClient();
+
+export default async function Home() {
+  const authToken = getServerToken();
+  const userResult = await getServerUser();
+  const reviewsResult = await serverApiClient.getReviews(authToken);
+
+  if (reviewsResult.isErr() || userResult.isErr()) {
+    return <></>;
+  }
+
   return (
     <main>
       <Container>
@@ -22,7 +35,7 @@ export default function Home() {
       </Container>
 
       <Container>
-        <Feed />
+        <Feed user={userResult.value} reviews={reviewsResult.value} />
       </Container>
     </main>
   );

0015d58f9f136d0f307c3ca4c5c1c6e88b61af1c
moaaz-bhnas <moaaz_bs@yahoo.com>
2023-10-18 09:14:27 +0300

    FIX

--- src/components/browse-reviews/LikersSkeleton.tsx
+++ src/components/browse-reviews/LikersSkeleton.tsx
@@ -6,7 +6,7 @@ function LikersSkeleton({}: Props) {
   return (
     <ul className="space-y-3">
       {skeletonItems.map((item) => (
-        <li className="flex items-center gap-2">
+        <li key={item} className="flex items-center gap-2">
           <div className="h-10 w-10 animate-pulse rounded-full bg-gray-300" />
           <div className="h-3 flex-1 animate-pulse bg-gray-300" />
         </li>
